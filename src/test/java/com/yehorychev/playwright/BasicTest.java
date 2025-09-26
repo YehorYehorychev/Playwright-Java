595ebe0561b9c6e0add7d418d5116f875a3d3562
Yehor Yehorychev <egor57899@gmail.com>
2025-09-25 17:13:19 -0700

    Fixures were updated to Annotation

--- src/test/java/com/yehorychev/playwright/BasicTest.java
+++ src/test/java/com/yehorychev/playwright/BasicTest.java
@@ -1,14 +1,34 @@
 package com.yehorychev.playwright;
 
-import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.AfterEach;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class BasicTest {
+
+    @Test
+    void shouldShowThePageTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com/");
+        String pageTitle = page.title();
+
+        Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void shouldSearchByKeyword(Page page) {
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("//input[@id='search-query']").fill("Pliers");
+        page.locator("//button[@class='btn btn-secondary']").click();
+
+        int matchingSearchResults = page.locator("//*[contains(text(),'Pliers')]").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+    }
+
+    // Legacy fixtures BEFORE -> @UsePlaywright
+    /*
     Playwright playwright;
     Browser browser;
     Page page;
@@ -25,23 +45,5 @@ public class BasicTest {
         browser.close();
         playwright.close();
     }
-
-    @Test
-    void shouldShowThePageTitle() {
-        page.navigate("https://practicesoftwaretesting.com/");
-        String pageTitle = page.title();
-
-        Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
-    }
-
-    @Test
-    void shouldSearchByKeyword() {
-        page.navigate("https://practicesoftwaretesting.com/");
-        page.locator("//input[@id='search-query']").fill("Pliers");
-        page.locator("//button[@class='btn btn-secondary']").click();
-
-        int matchingSearchResults = page.locator("//*[contains(text(),'Pliers')]").count();
-
-        Assertions.assertTrue(matchingSearchResults > 0);
-    }
+    */
 }
