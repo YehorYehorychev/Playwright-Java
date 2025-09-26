0e658d816eb2587d0a8cbee00b32955b8fa81bb9
Yehor Yehorychev <egor57899@gmail.com>
2025-09-26 14:50:12 -0700

    completeFormTest has been refactored

--- src/test/java/com/yehorychev/playwright/InputFieldsTest.java
+++ src/test/java/com/yehorychev/playwright/InputFieldsTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @UsePlaywright(HeadlessChromeOptions.class)
@@ -23,18 +27,34 @@ public class InputFieldsTest {
 
         @DisplayName("Input Fields")
         @Test
-        void completeFormTest(Page page) {
+        void completeFormTest(Page page) throws URISyntaxException {
             var firstNameField = page.getByLabel("First name");
-            var lastNameField  = page.getByLabel("Last name");
-            var emailField     = page.getByLabel("Email address");
+            var lastNameField = page.getByLabel("Last name");
+            var emailField = page.getByLabel("Email address");
+            var messageField = page.getByLabel("Message *");
+            var subjectSelector = page.getByLabel("Subject");
+            var uploadField = page.getByLabel("Attachment");
+
+            Path fileToUpload = Paths.get(ClassLoader
+                    .getSystemResource("data/test_data_for_upload_file_test.txt").toURI());
+
+            page.setInputFiles("#attachment", fileToUpload);
 
             firstNameField.fill("Yehor");
             lastNameField.fill("Yehorychev");
             emailField.fill("yehor@example.com");
+            messageField.fill("Hello World!");
+            subjectSelector.selectOption("payments");
 
             assertThat(firstNameField).hasValue("Yehor");
             assertThat(lastNameField).hasValue("Yehorychev");
             assertThat(emailField).hasValue("yehor@example.com");
+            assertThat(messageField).hasValue("Hello World!");
+            assertThat(subjectSelector).hasValue("payments");
+
+            String uploadedFile = uploadField.inputValue();
+            org.assertj.core.api.Assertions.assertThat(uploadedFile)
+                    .endsWith("upload_file_test.txt");
         }
     }
 }
