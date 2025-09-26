d30fbba1e16f60c55e3829e16f36b3f12792fa6a
Yehor Yehorychev <egor57899@gmail.com>
2025-09-25 17:01:03 -0700

    Playwright keywordSearch test has been added

--- src/test/java/com/yehorychev/playwright/BasicTest.java
+++ src/test/java/com/yehorychev/playwright/BasicTest.java
@@ -22,4 +22,21 @@ public class BasicTest {
         browser.close();
         playwright.close();
     }
+
+    void shouldSearchByKeyword() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("//input[@id='search-query']").fill("Pliers");
+        page.locator("//button[@class='btn btn-secondary']").click();
+
+        int matchingSearchResults = page.locator("//*[contains(text(),'Pliers')]").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+
+        browser.close();
+        playwright.close();
+    }
 }
