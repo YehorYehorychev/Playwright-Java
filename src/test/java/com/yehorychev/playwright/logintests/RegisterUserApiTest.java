1e9fee4d1976629917bdf8adf60b9666a55f3637
Yehor Yehorychev <egor57899@gmail.com>
2025-10-01 16:01:44 -0700

    shouldRegisterUser API Test has been added

--- src/test/java/com/yehorychev/playwright/logintests/RegisterUserApiTest.java
+++ src/test/java/com/yehorychev/playwright/logintests/RegisterUserApiTest.java
@@ -1,23 +1,26 @@
 package com.yehorychev.playwright.logintests;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.options.RequestOptions;
 import com.yehorychev.playwright.domain.User;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @UsePlaywright
 public class RegisterUserApiTest {
 
     private APIRequestContext request;
+    private Gson gson = new Gson();
 
     @BeforeEach
     void setup(Playwright playwright) {
@@ -34,24 +37,60 @@ public class RegisterUserApiTest {
         }
     }
 
+    @DisplayName("First Name is a mandatory field")
+    @Test
+    void firstNameIsMandatoryTest() {
+        User userWithNoName = User.randomUser().withFirstName(null);
+
+        var response = request.post("/users/register",
+                RequestOptions.create()
+                        .setHeader("Content-Type", "application/json")
+                        .setData(userWithNoName)
+        );
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(response.status()).isEqualTo(422);
+            JsonObject responseObject = gson.fromJson(response.text(), JsonObject.class);
+            softly.assertThat(responseObject.has("first_name")).isTrue();
+            String errorMessage = responseObject.get("first_name").getAsString();
+            softly.assertThat(errorMessage).isEqualTo("The first name field is required.");
+        });
+    }
+
     @DisplayName("Should register a new user")
     @Test
-    void shouldRegisterUser() {
+    void shouldRegisterUserTest() {
         User validUser = User.randomUser();
 
-        var response = request
-                .post("/users/register",
-                        RequestOptions.create()
-                                .setHeader("Content-Type", "application/json")
-                                .setData(validUser)
-                );
-
-        assertThat(response.status()).isEqualTo(201);
+        var response = request.post("/users/register",
+                RequestOptions.create()
+                        .setHeader("Content-Type", "application/json")
+                        .setData(validUser)
+        );
 
         String responseBody = response.text();
-        Gson gson = new Gson();
         User createdUser = gson.fromJson(responseBody, User.class);
 
-        assertThat(createdUser).isEqualTo(validUser.withPassword(null));
+        JsonObject responseObject = gson.fromJson(responseBody, JsonObject.class);
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(response.status())
+                    .as("Registration should return 201 created status code")
+                    .isEqualTo(201);
+
+            softly.assertThat(createdUser)
+                    .as("Created user should match the specified user without the password")
+                    .isEqualTo(validUser.withPassword(null));
+
+            assertThat(responseObject.has("password"))
+                    .as("No password should be returned")
+                    .isFalse();
+
+            softly.assertThat(responseObject.get("id").getAsString())
+                    .as("Registered user should have an id")
+                    .isNotEmpty();
+
+            softly.assertThat(response.headers().get("content-type")).contains("application/json");
+        });
     }
 }
