df88ef1e124634be17f4d20207b97151b6b1b668
Yehor Yehorychev <egor57899@gmail.com>
2025-10-06 08:48:54 -0700

    LoginPage has been added

--- src/test/java/com/yehorychev/playwright/login/LoginWithRegisteredUserTest.java
+++ src/test/java/com/yehorychev/playwright/login/LoginWithRegisteredUserTest.java
@@ -1,7 +1,14 @@
 package com.yehorychev.playwright.login;
 
 import com.yehorychev.playwright.helpers.PlaywrightTestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class LoginWithRegisteredUserTest extends PlaywrightTestCase {
 
+    @Test
+    @DisplayName("Should be able to login with a registered user")
+    void shouldLoginWithRegisteredUser() {
+
+    }
 }
