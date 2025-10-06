b2648881dbcf4d9e148be6ddca5c98352b48d112
Yehor Yehorychev <egor57899@gmail.com>
2025-10-06 08:56:07 -0700

    shouldLoginWithRegisteredUser test was created

--- src/test/java/com/yehorychev/playwright/login/LoginWithRegisteredUserTest.java
+++ src/test/java/com/yehorychev/playwright/login/LoginWithRegisteredUserTest.java
@@ -1,14 +1,23 @@
 package com.yehorychev.playwright.login;
 
+import com.yehorychev.playwright.domain.User;
 import com.yehorychev.playwright.helpers.PlaywrightTestCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LoginWithRegisteredUserTest extends PlaywrightTestCase {
 
     @Test
     @DisplayName("Should be able to login with a registered user")
     void shouldLoginWithRegisteredUser() {
+        User user = User.randomUser();
 
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.open();
+        loginPage.loginAs(user);
+
+        assertThat(loginPage.title()).isEqualTo("My account");
     }
 }
