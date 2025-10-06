b2633604aecfe65a7ceb93a4f3f82e1e3f53ed6d
Yehor Yehorychev <egor57899@gmail.com>
2025-10-06 08:39:37 -0700

    PlaywrightTestCase fixtures class has been added

---
+++ src/test/java/com/yehorychev/playwright/login/LoginWithRegisteredUserTest.java
@@ -0,0 +1,7 @@
+package com.yehorychev.playwright.login;
+
+import com.yehorychev.playwright.helpers.PlaywrightTestCase;
+
+public class LoginWithRegisteredUserTest extends PlaywrightTestCase {
+
+}
