c4051cc2c6936af02195ba4749c4c3902aeb02a6
Yehor Yehorychev <egor57899@gmail.com>
2025-10-10 15:48:41 -0700

    CucumberTestSuite class has been created

--- src/test/java/com/yehorychev/cucumber/stepdefinitions/ProductCatalogStepDefinitions.java
+++ src/test/java/com/yehorychev/cucumber/stepdefinitions/ProductCatalogStepDefinitions.java
@@ -6,19 +6,16 @@ public class ProductCatalogStepDefinitions {
 
     @Given("Sally is on the home page")
     public void sally_is_on_the_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("she searches for {string}")
     public void she_searches_for(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("the {string} product should be displayed")
     public void the_product_should_be_displayed(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
