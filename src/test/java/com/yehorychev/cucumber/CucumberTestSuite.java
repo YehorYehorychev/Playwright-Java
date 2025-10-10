1550d8a76725f1b551ddc89b3a3a79ba7b918d73
Yehor Yehorychev <egor57899@gmail.com>
2025-10-10 15:37:39 -0700

    Cucumber feature file has been created as a refenrence for the future impl

---
+++ src/test/java/com/yehorychev/cucumber/CucumberTestSuite.java
@@ -0,0 +1,5 @@
+package com.yehorychev.cucumber;
+
+public class CucumberTestSuite {
+
+}
---
+++ src/test/java/com/yehorychev/cucumber/stepdefinitions/ProductCatalogStepDefinitions.java
@@ -0,0 +1,24 @@
+package com.yehorychev.cucumber.stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class ProductCatalogStepDefinitions {
+
+    @Given("Sally is on the home page")
+    public void sally_is_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("she searches for {string}")
+    public void she_searches_for(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the {string} product should be displayed")
+    public void the_product_should_be_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
